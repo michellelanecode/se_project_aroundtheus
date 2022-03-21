61a6ccbb34612b52c4e8a1a9e33d61c9e294914f
michellelanecode <michellecolemanlane@gmail.com>
2022-03-21 13:03:50 -0400

    Modal style complete
    Modal window opens and closes

--- styles/script.js
+++ styles/script.js
@@ -1,3 +1,25 @@
 var editButton = document.querySelector('.profile__buttons_edit')
+var popup = document.querySelector('.popup')
+var closeButton = document.querySelector('.popup__form-close')
+var profileName =;
+var aboutMe =;
+
+//update about me with information put into modal window
+
+
+
+
+
 console.log(editButton);
 //onclick, change display on the edit button to block instead of hidden
+
+console.log(popup);
+editButton.addEventListener('click', function (event){
+		event.preventDefault();
+		popup.style.display = 'block';
+})
+
+closeButton.addEventListener('click', function (event) {
+	event.preventDefault();
+		popup.style.display = 'none';
+})

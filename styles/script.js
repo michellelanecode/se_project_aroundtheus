7d0bc5a6cbbf9be68195156273651862c52517e6
michellelanecode <michellecolemanlane@gmail.com>
2022-03-21 13:32:31 -0400

    modal window working

--- styles/script.js
+++ styles/script.js
@@ -1,19 +1,12 @@
 var editButton = document.querySelector('.profile__buttons_edit')
 var popup = document.querySelector('.popup')
 var closeButton = document.querySelector('.popup__form-close')
-var profileName =;
-var aboutMe =;
+var profileName = document.querySelector('.profile__title');
+var aboutMe = document.querySelector('.profile__subtitle');
+var modalWindow = document.querySelector('.popup__form')
+var first = document.getElementById('first')
+var last = document.getElementById('last')
 
-//update about me with information put into modal window
-
-
-
-
-
-console.log(editButton);
-//onclick, change display on the edit button to block instead of hidden
-
-console.log(popup);
 editButton.addEventListener('click', function (event){
 		event.preventDefault();
 		popup.style.display = 'block';
@@ -21,5 +14,13 @@ editButton.addEventListener('click', function (event){
 
 closeButton.addEventListener('click', function (event) {
 	event.preventDefault();
+		popup.style.display = 'none';
+
+})
+
+modalWindow.addEventListener('submit', function (event){
+		event.preventDefault();
+		profileName.innerHTML = first.value;
+		aboutMe.innerHTML = last.value;
 		popup.style.display = 'none';
 })

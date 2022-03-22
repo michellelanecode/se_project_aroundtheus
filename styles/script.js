8362b8b5ceef3e29f10b76a6b39cc18e9f4b048e
michellelanecode <michellecolemanlane@gmail.com>
2022-03-22 02:30:07 -0400

    submitting for first review
    submitting

--- styles/script.js
+++ styles/script.js
@@ -1,26 +1,33 @@
-var editButton = document.querySelector('.profile__buttons-edit')
-var popup = document.querySelector('.popup')
-var closeButton = document.querySelector('.popup__form-close')
-var profileName = document.querySelector('.profile__title');
-var aboutMe = document.querySelector('.profile__subtitle');
-var modalWindow = document.querySelector('.popup__form')
-var first = document.getElementById('first')
-var last = document.getElementById('last')
+var editButton = document.querySelector(".profile__buttons-edit");
+var popup = document.querySelector(".popup");
+var closeButton = document.querySelector(".popup__form-close");
+var profileName = document.querySelector(".profile__title");
+var aboutMe = document.querySelector(".profile__subtitle");
+var modalWindow = document.querySelector(".popup__form");
+var first = document.getElementById("first");
+var last = document.getElementById("last");
+var loveButton = document.querySelector(".card__lovebutton");
 
-editButton.addEventListener('click', function (event){
-		event.preventDefault();
-		popup.style.display = 'block';
-})
+editButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  popup.style.display = "block";
+});
 
-closeButton.addEventListener('click', function (event) {
-	event.preventDefault();
-		popup.style.display = 'none';
+closeButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  popup.style.display = "none";
+});
 
-})
+modalWindow.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profileName.innerHTML = first.value;
+  aboutMe.innerHTML = last.value;
+  popup.style.display = "none";
+});
 
-modalWindow.addEventListener('submit', function (event){
-		event.preventDefault();
-		profileName.innerHTML = first.value;
-		aboutMe.innerHTML = last.value;
-		popup.style.display = 'none';
-})
+loveButton.addEventListener("click", function (event) {
+  if (loveButton.className === "card__lovebutton_active") {
+    loveButton.className = "card__lovebutton";
+  }
+  loveButton.className = "card__lovebutton_active";
+});

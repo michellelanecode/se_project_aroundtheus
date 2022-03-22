88161c75a7b81bb3db8301a2b939fded1f18e229
michellelanecode <michellecolemanlane@gmail.com>
2022-03-22 18:45:38 -0400

    Revision 2
    modal window restyled, hover styling added, close button positioned, js updated.

--- scripts/script.js
+++ scripts/script.js
@@ -7,33 +7,34 @@ var modalWindow = document.querySelector(".popup__form");
 var first = document.getElementById("first");
 var last = document.getElementById("last");
 var loveButton = document.querySelectorAll(".card__lovebutton");
+
+
 editButton.addEventListener("click", function (event) {
   event.preventDefault();
-  popup.style.display = "block";
+  popup.style.visibility= "visible";
 });
 
 closeButton.addEventListener("click", function (event) {
   event.preventDefault();
-  popup.style.display = "none";
+  popup.style.visibility = "hidden";
 });
 
 modalWindow.addEventListener("submit", function (event) {
   event.preventDefault();
   profileName.innerHTML = first.value;
   aboutMe.innerHTML = last.value;
-  popup.style.display = "none";
+  popup.style.visibility= "hidden";
 });
 
-  loveButton.forEach(function (node) {
-    node.addEventListener('click', function (event){
-        if (node.className === "card__lovebutton"){
-            node.className = "card__lovebutton_active";
-        } else {
-            node.className = "card__lovebutton"
-        }
-    })
-
-})
+loveButton.forEach(function (node) {
+  node.addEventListener("click", function (event) {
+    if (node.classList.contains("card__lovebutton_disabled")) {
+      node.className  = "card__lovebutton_active card__lovebutton"
+    } else {
+      node.className  = "card__lovebutton_disabled card__lovebutton"
+    }
+  });
+});
 
 // loveButton.addEventListener("click", function (event) {
 

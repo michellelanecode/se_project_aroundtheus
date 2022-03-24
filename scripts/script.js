c6d07a38458fd0210467948709687db8aafc2305
michellelanecode <michellecolemanlane@gmail.com>
2022-03-23 20:55:50 -0400

    Revision complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,48 @@
-var editButton = document.querySelector(".profile__buttons-edit");
-var popup = document.querySelector(".popup");
-var closeButton = document.querySelector(".popup__form-close");
-var profileName = document.querySelector(".profile__title");
-var aboutMe = document.querySelector(".profile__subtitle");
-var modalWindow = document.querySelector(".popup__form");
-var first = document.getElementById("first");
-var last = document.getElementById("last");
-var loveButton = document.querySelectorAll(".card__lovebutton");
+const editButton = document.querySelector(".profile__buttons-edit");
+const popup = document.querySelector(".popup");
+const closeButton = document.querySelector(".popup__form-close");
+const profileName = document.querySelector(".profile__title");
+const aboutMe = document.querySelector(".profile__subtitle");
+const saveButton = document.querySelector(".popup__form-button");
+const modalWindow = document.querySelector(".popup__form");
+const loveButtons = document.querySelectorAll(".card__lovebutton");
+const name = document.querySelector(".popup__form-field_name");
+const aboutInfo = document.querySelector(".popup__form-field_about");
 
+const popUp = function () {
+  console.log("running");
+  popup.style.visibility = "visible";
+};
 
-editButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  popup.style.visibility= "visible";
-});
-
-closeButton.addEventListener("click", function (event) {
-  event.preventDefault();
+const closePopup = function () {
+  name.value = "";
+  aboutInfo.value = "";
   popup.style.visibility = "hidden";
-});
+};
 
-modalWindow.addEventListener("submit", function (event) {
+const updateProfile = function () {
   event.preventDefault();
-  profileName.innerHTML = first.value;
-  aboutMe.innerHTML = last.value;
-  popup.style.visibility= "hidden";
+  profileName.textContent = name.value;
+  aboutMe.textContent = aboutInfo.value;
+  name.value = "";
+  aboutInfo.value = "";
+  popup.style.visibility = "hidden";
+};
+
+const loveButtonUpdate = function () {
+  if (this.className === "card__lovebutton card__lovebutton_disabled") {
+    this.className = "card__lovebutton card__lovebutton_active";
+  } else {
+    this.className = "card__lovebutton card__lovebutton_disabled";
+  }
+};
+
+editButton.addEventListener("click", popUp);
+
+closeButton.addEventListener("click", closePopup);
+
+modalWindow.addEventListener("submit", updateProfile);
+
+loveButtons.forEach(function (button) {
+  button.addEventListener("click", loveButtonUpdate);
 });
-
-loveButton.forEach(function (node) {
-  node.addEventListener("click", function (event) {
-    if (node.classList.contains("card__lovebutton_disabled")) {
-      node.className  = "card__lovebutton_active card__lovebutton"
-    } else {
-      node.className  = "card__lovebutton_disabled card__lovebutton"
-    }
-  });
-});
-
-// loveButton.addEventListener("click", function (event) {
-
-//   if (loveButton.className === "card__lovebutton_active") {
-//     loveButton.className = "card__lovebutton";
-//   }
-//   loveButton.className = "card__lovebutton_active";
-// });

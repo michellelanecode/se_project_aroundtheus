dec81f12819b863e161afa0a4a72716c550354d3
Monique Lane <michellecolemanlane@gmail.com>
2022-04-09 05:28:25 -0400

    working on popup transitions
    popups are currently disabled while working on smooth opening and closing transition

--- scripts/script.js
+++ scripts/script.js
@@ -8,31 +8,30 @@ const name = document.querySelector('.popup__form-field_type_name');
 const imageTitle = document.querySelector('.popup__form-field_type_title');
 const imageUrl = document.querySelector('.popup__form-field_type_link');
 // card functionality
-const initialCards = [
-  {
-    name: "Scranton",
-    link: "images/Scranton.jpeg"
-  },
-  {
-    name: "Silicon Valley",
-    link: "images/SiliconValley.png"
-  },
-  {
-    name: "E Corp",
-    link: "images/ECorp.jpeg"
-  },
-  {
-    name: "Downton",
-    link: "images/Downton.jpeg"
-  },
-  {
-    name: "Pawnee",
-    link: "images/pawnee.jpeg"
-  },
-  {
-    name: "Disney World",
-    link: "images/disneyworld.jpeg"
-  }
+const initialCards = [{
+        name: "Scranton",
+        link: "images/Scranton.jpeg"
+    },
+    {
+        name: "Silicon Valley",
+        link: "images/SiliconValley.png"
+    },
+    {
+        name: "E Corp",
+        link: "images/ECorp.jpeg"
+    },
+    {
+        name: "Downton",
+        link: "images/Downton.jpeg"
+    },
+    {
+        name: "Pawnee",
+        link: "images/pawnee.jpeg"
+    },
+    {
+        name: "Disney World",
+        link: "images/disneyworld.jpeg"
+    }
 ];
 
 //creating cards
@@ -45,32 +44,34 @@ const cards = document.querySelector('.cards');
 const photoPopup = document.querySelector('.popup__image-photo');
 
 
-const loveButtonUpdate = function () {
-  this.classList.toggle("card__lovebutton_active");
+const loveButtonUpdate = function() {
+    this.classList.toggle("card__lovebutton_active");
 };
 
-const openPhoto = function () {
-  imagePopup.style.display = 'block';
-  photoPopup.src = this.src;
-  popupDiv.style.visibility = "visible";
+const openPhoto = function() {
+    var title = this.parentNode.querySelector('.card__caption').textContent;
+    document.querySelector('.popup__image-text').textContent = title;
+    imagePopup.style.display = 'block';
+    photoPopup.src = this.src;
+    popupDiv.style.visibility = "visible";
 }
 
-const deletePhoto = function () {
-  this.parentNode.remove();
+const deletePhoto = function() {
+    this.parentNode.remove();
 }
 
-const cardRender = function (card) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = card.link;
-  cardElement.querySelector('.card__caption').textContent = card.name;
-  cardElement.querySelector('.card__lovebutton').addEventListener('click', loveButtonUpdate);
-  cardElement.querySelector('.card__image').addEventListener('click', openPhoto);
-  cardElement.querySelector('.card__deletebutton').addEventListener('click', deletePhoto);
-  cards.prepend(cardElement);
+const cardRender = function(card) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__caption').textContent = card.name;
+    cardElement.querySelector('.card__lovebutton').addEventListener('click', loveButtonUpdate);
+    cardElement.querySelector('.card__image').addEventListener('click', openPhoto);
+    cardElement.querySelector('.card__deletebutton').addEventListener('click', deletePhoto);
+    cards.prepend(cardElement);
 };
 
 initialCards.forEach(card => {
-  cardRender(card);
+    cardRender(card);
 })
 
 
@@ -85,43 +86,42 @@ const closeButton = document.querySelectorAll('.popup__form-close')
 
 
 //create an element for the like button everytime a card renders
-const showEdit = function () {
-  name.value = profileName.textContent;
-  aboutInfo.value = aboutName.textContent;
-  popupDiv.style.visibility = "visible";
-  editPopup.style.display = 'block'
+const showEdit = function() {
+    name.value = profileName.textContent;
+    aboutInfo.value = aboutName.textContent;
+    popupDiv.style.visibility = "visible";
+    editPopup.style.opacity = 1;
+    popupDiv.style.opacity = 1;
 }
 
-const showAdd = function () {
-  popupDiv.style.visibility = "visible";
-  addPopup.style.display = 'block'
+const showAdd = function() {
+    popupDiv.style.visibility = "visible";
+    addPopup.style.display = 'block'
 }
 
-const closePopup = function () {
-  popupDiv.style.visibility = "hidden";
-  addPopup.style.display = 'none'
-  editPopup.style.display = 'none'
-  imagePopup.style.display = 'none'
+const closePopup = function() {
+    popupDiv.style.visibility = "hidden";
+
 }
 
 
 
-const updateProfile = function (event) {
-  event.preventDefault();
-  profileName.textContent = name.value;
-  aboutName.textContent = aboutInfo.value;
-  closePopup();
+const updateProfile = function(event) {
+    event.preventDefault();
+    profileName.textContent = name.value;
+    aboutName.textContent = aboutInfo.value;
+    closePopup();
 };
 
 
-const createCard = function (event) {
-  event.preventDefault();
-  //grab information add to object and add to beginnning of array
-  var obj = { name: imageTitle.value, link: imageUrl.value };
-  initialCards.unshift(obj);
-  cardRender(obj)
-  closePopup();
-  document.querySelector('.popup__form_type_add').reset();
+const createCard = function(event) {
+    event.preventDefault();
+    //grab information add to object and add to beginnning of array
+    var obj = { name: imageTitle.value, link: imageUrl.value };
+    initialCards.unshift(obj);
+    cardRender(obj)
+    closePopup();
+    document.querySelector('.popup__form_type_add').reset();
 }
 
 
@@ -129,27 +129,24 @@ const createCard = function (event) {
 //events
 
 form.forEach(form => {
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    if (event.target.classList.contains('popup__form_type_add')) {
-      createCard(event)
-    } else {
-      updateProfile(event)
-    }
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        if (event.target.classList.contains('popup__form_type_add')) {
+            createCard(event)
+        } else {
+            updateProfile(event)
+        }
 
-  });
+    });
 })
 
-loveButtons.forEach(function (button) {
-  button.addEventListener("click", loveButtonUpdate);
+loveButtons.forEach(function(button) {
+    button.addEventListener("click", loveButtonUpdate);
 });
 editButton.addEventListener('click', showEdit)
 addButton.addEventListener('click', showAdd);
 closeButton.forEach((button) => {
-  button.addEventListener('click', function (event) {
-    closePopup();
-  })
+    button.addEventListener('click', function(event) {
+        closePopup();
+    })
 })
-
-
-

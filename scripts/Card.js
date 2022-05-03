b409cd372c38d8b95a02351bc7f8edfbd9368000
Monique Lane <michellecolemanlane@gmail.com>
2022-05-02 23:01:45 -0400

    added new files

---
+++ scripts/Card.js
@@ -0,0 +1,12 @@
+class Card {
+    constructor(data, cardSelector) {
+        this._text = data.text;
+        this._link = data.link
+    }
+
+
+
+
+}
+
+export default Card;
---
+++ scripts/FormValidator.js
@@ -0,0 +1,7 @@
+class FormValidator {
+    constructor(settingsObj, formElement) {
+        //settings object that stores selectors and form classes, and form element to be validated.
+    }
+}
+
+export default FormValidatior;
